c6e77f4dbebbe20ddcd57c3e4650a468b1f9b006
이정윤 <leeyun1533@naver.com>
2020-04-26 19:47:20 +0900

    Card component 이미지부분 보강

--- src/components/CardUI.vue
+++ src/components/CardUI.vue
@@ -2,7 +2,7 @@
   <div class="card" :class="horizontal ? 'horizontal-style' : ''">
     <div class="card-body" :style="bodyStyle">
       <slot>
-        <img :src="imageUrl" alt />
+        <div class="background-section" :style="{'background-image': `url(${imageUrl})`}"></div>
         <div>
           <div class="body-main-container">
             <span class="label">{{label}}</span>
@@ -33,9 +33,9 @@ export default {
   data() {
     return {
       rate: null
-    }
+    };
   }
-}
+};
 </script>
 <style lang="scss" scoped>
 .card {
@@ -43,17 +43,19 @@ export default {
   flex-direction: column;
   width: 33%;
   min-width: 200px;
-  max-width: 300px;
+  max-width: 270px;
   border-radius: 4px;
   border: 1px solid #e4e7ed;
   background-color: white;
   overflow: hidden;
   color: #303133;
   transition: 0.3s;
+  margin: 12px;
 
-  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  transition: all 0.2s ease-in-out;
   &:hover,
   &:focus {
+    transform: scale(1.02);
     box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
   }
 
@@ -66,8 +68,9 @@ export default {
         flex-direction: column;
         justify-content: space-between;
       }
-      > img {
-        max-width: 200px;
+      .background-section {
+        min-width: 200px;
+        height: 200px;
       }
       .body-footer-container {
         border: none;
@@ -77,15 +80,16 @@ export default {
     max-width: 600px;
   }
   .card-body {
-    img {
-      width: 100%;
-      min-width: 200px;
-      max-height: 300px;
+    .background-section {
+      min-width: 150px;
+      height: 230px;
+      background-size: cover;
     }
     .body-main-container {
       display: flex;
       flex-direction: column;
       padding: 8px 12px;
+      height: 90px;
       .label {
         font-size: 14px;
         color: #eeeeee;
